848f8903cf2d8da3587667b53fabac41667b64b3
zhiyuanFu <fzy997@sina.com>
2023-11-08 12:06:10 +0000

    Signed-off-by: zhiyuanFu <fzy997@sina.com>

--- g17icn.py
+++ g17icn.py
@@ -68,16 +68,6 @@ class G17ICNNODE:
     async def start(self): 
         pass
 
-
-    '''
-    Handle a request for a interested. 
-    When it get a request from other nodes, it will at first check the self.PIT to find if there is a same request, if exist, do not pass this request, and save this request to waiting list.
-    When a request interested come back to this node, it should check the time validity, if not valid, discard it. if valid, return this interested message to the requestor and nodes in the waiting list.
-    It will check its` cache, if there is an entry suitable to this interested, return it. If not, it will use the self.FIB to pass this request to the next hop and save this request to its pit.
-    '''
-    def HandleInterested():
-        pass
-
     '''
     Handle a request for a interested. 
     When it get a request from other nodes, it will at first check the self.PIT to find if there is a same request, if exist, do not pass this request, and save this request to waiting list.
