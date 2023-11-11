af52ca143b23256eccfc95c9cca29ff81a547a31
Naman Arora <59131920+ArorAnam@users.noreply.github.com>
2023-11-11 16:29:25 +0000

    some change

--- routing.py
+++ routing.py
@@ -1,8 +1,13 @@
 '''
 All the forwarding logic will go here
 Will absatract away from the Node file
+
+<NamaN ArorA TASK>
 '''
 
 class Routing:
 
-    pass
+    def get_neighbours():
+        neighbour_list = []
+
+        return neighbour_list
