4c546f4053ec64285bf3c41aa51e50ddb2b6c993
Neimhin Christopher Robinson Gunning <nrobinso@tcd.ie>
2023-11-16 11:26:11 +0000

    encapsulate http

--- encapsulate_http.py
+++ encapsulate_http.py
@@ -1,13 +1,13 @@
 import socket
-def encapsulate_http_request(path, tcp_host, tcp_port):
+def http_request(path, tcp_host, tcp_port, method='GET'):
     request_headers = [
-        f"GET {path} HTTP/1.1",
+        f"{method} {path} HTTP/1.1",
         f"Host: {tcp_host}",
         "Connection: close",
         "\r\n"
     ]
 
-    # Create a TCP socket
+    # create socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((tcp_host, tcp_port))
 
@@ -15,19 +15,19 @@ def encapsulate_http_request(path, tcp_host, tcp_port):
     request_data = "\r\n".join(request_headers).encode()
     s.sendall(request_data)
 
-    # Receive the response headers
+    # read headers from tcp stream 
     response_headers = ''
     while '\r\n\r\n' not in response_headers:
         response_headers += s.recv(4096).decode()
 
-    # Extract content length
+    # content length header
     content_length = None
     for header in response_headers.split('\r\n'):
         if header.lower().startswith('content-length'):
             content_length = int(header.split(':')[1].strip())
             break
 
-    # Read the response body
+    # read response body according to content-length
     response_body = ''
     if content_length:
         while len(response_body) < content_length:
@@ -37,29 +37,19 @@ def encapsulate_http_request(path, tcp_host, tcp_port):
 
     return response_headers + response_body
 
-# def encapsulate_http_request(path, tcp_host, tcp_port):
-#     # HTTP request headers
-#     request_headers = [
-#         f"GET {path} HTTP/1.1",
-#         f"Host: {tcp_host}",
-#         "Connection: close",
-#         "\r\n"
-#     ]
-# 
-#     # Create a TCP socket
-#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     s.connect((tcp_host, tcp_port))
-# 
-#     # Send the HTTP request as raw data
-#     request_data = "\r\n".join(request_headers).encode()
-#     s.sendall(request_data)
-# 
-#     # Receive the response (this is simplified, real-world use requires more robust handling)
-#     response = s.recv(4096)
-#     s.close()
-# 
-#     return response
+def extract_body_from_response(http_response):
+    # split the response into headers and body
+    parts = http_response.split('\r\n\r\n', 1)
+
+    # check if the split found both headers and body
+    if len(parts) > 1:
+        headers, body = parts
+        return body
+    else:
+        # no body, return an empty string
+        return ''
 
 if __name__ == "__main__":
-        response = encapsulate_http_request('http://10.35.70.37:33000', '10.35.70.37', 33000)
+        response = http_request('/', '10.35.70.37', 33000)
         print(response)
+        print("RESPONSE BODY\n", extract_body_from_response(response))
