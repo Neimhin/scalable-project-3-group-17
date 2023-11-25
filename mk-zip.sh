645ae4002601a06284cc5f87bb2c31b88b0a94ee
Neimhin <nrobinso@tcd.ie>
2023-11-25 23:09:12 +0000

    runme.sh in zip

--- mk-zip.sh
+++ mk-zip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip -r g17icn-code-project-3.zip src/* requirements.txt mk-zip.sh debug/* 
+zip -r g17icn-code-project-3.zip src/* requirements.txt mk-zip.sh debug/* README.md runme.sh INSTALL.md
